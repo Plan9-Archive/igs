346448fa608531223f2a3fc3bc348698f8ab68a5
otter9space <otter9space@gmail.com>
2019-07-11 17:08:58 +0200

    Add goban traditional dimensions.

--- goban.c
+++ goban.c
@@ -3,6 +3,18 @@
 #include <draw.h>
 #include <event.h>
 
+/* Traditional dimensions in tenth of mm */
+enum
+{
+	Gobanw = 4242,
+	Gobanh = 4545,
+	Line = 10,
+	Linew = 220,
+	Lineh = 237,
+	Hoshidiam = 40,
+	Stonediam = 225
+};
+
 void
 main(void)
 {
