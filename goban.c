268e762a321501b6448f47605bb761802398399c
otter9space <otter9space@gmail.com>
2019-07-11 17:09:30 +0200

    Draw goban border.

--- goban.c
+++ goban.c
@@ -16,6 +16,22 @@ enum
 };
 
 void
+drawgoban(int size, s8int *goban)
+{
+	int n;
+	double scale;
+	Point o;
+	Rectangle r;
+
+	o = screen->r.min;
+	n = Dx(screen->r) < Dy(screen->r) ? Dx(screen->r) : Dy(screen->r);
+	scale = (double)n / (double)Gobanh;
+
+	r = Rect(o.x, o.y, o.x + n, o.y + n);
+	border(screen, r, 1, display->black, ZP);
+}
+
+void
 main(void)
 {
 	Mouse m;
@@ -23,6 +39,8 @@ main(void)
 	if(initdraw(0, 0, "goban") < 0)
 		sysfatal("initgoban failed: %r");
 	einit(Emouse);
+
+	drawgoban(5, nil);
 
 	for(;;m = emouse())
 		if(m.buttons)
