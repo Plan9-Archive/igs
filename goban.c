608e5d90c9651637c725cb9fa55c245c10755b21
otter9space <otter9space@gmail.com>
2019-07-11 17:08:15 +0200

    Add draw & mouse init.

--- goban.c
+++ goban.c
@@ -2,3 +2,27 @@
 #include <libc.h>
 #include <draw.h>
 #include <event.h>
+
+void
+main(void)
+{
+	Mouse m;
+
+	if(initdraw(0, 0, "goban") < 0)
+		sysfatal("initgoban failed: %r");
+	einit(Emouse);
+
+	for(;;m = emouse())
+		if(m.buttons)
+			break;
+
+	exits(0);
+}
+
+void
+eresized(int new)
+{
+	if(new)
+		if(getwindow(display, Refnone) < 0)
+			sysfatal("eresized failed: %r");
+}
