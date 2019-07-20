e61cc888875eaed78f3fe29ab94dd4329aecb307
otter9space <otter9space@gmail.com>
2019-07-20 15:24:06 +0200

    Replace 19 by sgoban in move & coord conversions.

--- telnet.c
+++ telnet.c
@@ -1,20 +1,24 @@
 #include <u.h>
 #include <libc.h>
+#include <thread.h>
 
 void
-main(void)
+threadmain(int argc, char *argv[])
 {
 	int telfd, n;
 	char *c, buf[512];
 	char devdir[NETPATHLEN];
 
 	telfd = dial("net!igs.joyjoy.net!7777", 0, devdir, 0);
-	print("%d %s\n", telfd, devdir);
+	if(telfd < 0)
+		sysfatal("dial: %r");
 
 	for(;;){
 		n = read(telfd, buf, sizeof(buf));
-		for(c = buf; c < buf + n; c++)
-			print("%c", *c);
+		for(c = buf; c < buf + n; c++){
+			if(*c != '\r')
+				print("%c", *c);
+		}
 	}
 
 	exits(0);
